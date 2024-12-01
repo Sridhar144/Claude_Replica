70a9afaa626cd484c0cdbbae9c8467467f4a2d5f
Sridhar <sridharsundar100@gmail.com>
2024-12-01 21:09:19 +0530

    something

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase Config
 export const firebaseConfig = {
-  apiKey: "AIzaSyBoDlAyJdwqPqFH0fhFz6j15NRslb2VUuU",
-  authDomain: "project-b93a3.firebaseapp.com",
-  projectId: "project-b93a3",
-  storageBucket: "project-b93a3.firebasestorage.app",
-  messagingSenderId: "665514280750",
-  appId: "1:665514280750:web:d583ab20fb45d633651cbf",
-  measurementId: "G-YRGBCE57CY"
+
 };
 
 const app = initializeApp(firebaseConfig);
